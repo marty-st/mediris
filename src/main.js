42a66e6f2c2f02d774f41e6da5302818d766c4b4
Martin Štourač <525032@mail.muni.cz>
2025-09-02 16:36:58 +0200

    feat basic webgl canvas and twgl import

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-"use strict"
+'use strict'
+
+import * as twgl from 'twgl.js';
+// dicomParser will require either include via CDN (or local copy) or a use of a bundler
+// e.g. Webpack or Vite
+// import * as dp from '../node_modules/dicom-parser/dist/dicomParser.js'
