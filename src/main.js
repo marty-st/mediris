a20247865d46290b2ebf151f9fc8d1162e4b2d4a
Martin Štourač <525032@mail.muni.cz>
2025-09-02 18:07:50 +0200

    feat add dicomParser as global object

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
 'use strict'
 
 import * as twgl from 'twgl.js';
-// dicomParser will require either include via CDN (or local copy) or a use of a bundler
-// e.g. Webpack or Vite
-// import * as dp from '../node_modules/dicom-parser/dist/dicomParser.js'
