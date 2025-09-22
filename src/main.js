36fbbcbe7926916cf86a0d60908852fa52d33008
Martin Štourač <525032@mail.muni.cz>
2025-09-22 22:37:59 +0200

    feat render of individual volume slices to viewport

--- src/main.js
+++ src/main.js
@@ -16,7 +16,12 @@ import render from './webgl/render.js';
 let canvas = undefined;             // HTML <canvas> element 
 let gl = undefined;                 // WebGL rendering context element
 let pane = undefined;               // Tweakpane rendering window
+let geometries = [];
 let volumeTexture = null;
+
+let UIData = {
+  slice: 1,
+};
 
 /* --------------------- */
 /* ----FILE LOADING----- */
@@ -31,7 +36,8 @@ window.onload = async function init()
   /* --UI INITIALIZATION-- */
   /* --------------------- */
 
-  pane = initDebugUI();
+  pane = initDebugUI(UIData);
+  // console.log("pane state", pane.exportState());
 
   /* --------------------- */
   /* CANVAS INITIALIZATION */
@@ -46,7 +52,8 @@ window.onload = async function init()
   /* SHADER INITIALIZATION */
   /* --------------------- */
 
-  const programInfo = await createShaderProgram(gl, "basic");
+  const triangleProgramInfo = await createShaderProgram(gl, "basic");
+  const volumeProgramInfo = await createShaderProgram(gl, "fsquad", "slice_texture");
 
   /* --------------------- */
   /* -DATA INITIALIZATION- */
@@ -56,50 +63,84 @@ window.onload = async function init()
      position: { numComponents: 2, data: [-0.5, 0, 0, 0.866, 0.3, 0], },
   };
 
-  const triangleBufferInfo = twgl.createBufferInfoFromArrays(gl, triangleArrays);
-  const triangleVAO = twgl.createVAOFromBufferInfo(gl, programInfo, triangleBufferInfo);
+  // Hack to be able to use twgl.drawBufferInfo() so that it behaves the same as
+  // gl.drawArrays(gl.TRIANGLES, 0, 3)
+  // where this setup sets bufferInfo.numElements to 3
+  const fullScreenQuadArrays = {
+    position: { numComponents: 1, data: [0, 0, 0], },
+  };
 
-  const geometries = [ {bufferInfo: triangleBufferInfo, vao: triangleVAO, programInfo: programInfo} ];
+  const triangleBufferInfo = twgl.createBufferInfoFromArrays(gl, triangleArrays);
+  const triangleVAO = twgl.createVAOFromBufferInfo(gl, triangleProgramInfo, triangleBufferInfo);
+
+  const fullScreenQuadBufferInfo = twgl.createBufferInfoFromArrays(gl, fullScreenQuadArrays);
+  const emptyVAO = twgl.createVAOFromBufferInfo(gl, volumeProgramInfo, fullScreenQuadBufferInfo);
+
+  geometries.push(
+    {bufferInfo: triangleBufferInfo, vao: triangleVAO, programInfo: triangleProgramInfo, uniforms: null},
+  );
 
   // Asynchronously load DICOM to display later
   imageDataPromise.then((imageData) => {
 
-  console.log("DICOM:", imageData);
-  const dimensions = imageData.dimensions;
-  const volume = imageData.volume;
+    console.log("DICOM:", imageData);
+    const dimensions = imageData.dimensions;
+    const volume = imageData.volume;
 
-  // Example: upload 3D texture (unsigned 16-bit)
-  volumeTexture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_3D, volumeTexture);
-  gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
-  gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
+    // Example: upload 3D texture (unsigned 16-bit)
+    volumeTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_3D, volumeTexture);
+    gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
+    gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
 
-  
+    // Choose formats based on bit depth and signedness (example: unsigned 16)
+    gl.texImage3D(
+      gl.TEXTURE_3D,
+      0,
+      gl.R16UI,             // internalFormat
+      dimensions.cols,
+      dimensions.rows,
+      dimensions.depth,
+      0,
+      gl.RED_INTEGER,       // format
+      gl.UNSIGNED_SHORT,    // type
+      volume
+    );
 
-  // Choose formats based on bit depth and signedness (example: unsigned 16)
-  gl.texImage3D(
-    gl.TEXTURE_3D,
-    0,
-    gl.R16UI,             // internalFormat
-    dimensions.cols,
-    dimensions.rows,
-    dimensions.depth,
-    0,
-    gl.RED_INTEGER,       // format
-    gl.UNSIGNED_SHORT,    // type
-    volume
-  );
+    geometries.push({
+      bufferInfo: fullScreenQuadBufferInfo, 
+      vao: emptyVAO, 
+      programInfo: volumeProgramInfo, 
+      uniforms: {
+        u_volume_texture: volumeTexture,
+        u_slice_number: UIData.slice,
+        u_slice_count: dimensions.depth,
+      }
+    });
 
+    // render again with the new geometry loaded
+    this.requestAnimationFrame(renderLoop);
   })
   
   /* --------------------- */
   /* -----RENDER LOOP----- */
   /* --------------------- */
 
-  // TODO: render texture data
+  // Renders before DICOM data are loaded
+  render(gl, canvas, geometries);
+}
+
+function update()
+{
+  geometries[1].uniforms.u_slice_number = UIData.slice;
+}
+
+export function renderLoop()
+{
+  update();
   render(gl, canvas, geometries);
 }
