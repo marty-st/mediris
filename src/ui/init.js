7983e6eb190bc0d49b059d76f7f29703da7ab56d
Martin Štourač <525032@mail.muni.cz>
2025-09-22 22:36:39 +0200

    feat UI slider for texture slices

--- src/ui/init.js
+++ src/ui/init.js
@@ -6,19 +6,29 @@ import { Pane } from 'tweakpane';
  * Initializes the context of Tweakpane UI elements for debugging purposes
  * @returns `Pane`object
  */
-export function initDebugUI()
+export function initDebugUI(UIData)
 {
   const pane = new Pane();
 
+  // const PARAMS = {
+  //   factor: 123,
+  //   title: 'hello',
+  //   color: '#ff0055',
+  // };
+
+  // pane.addBinding(PARAMS, 'factor');
+  // pane.addBinding(PARAMS, 'title');
+  // pane.addBinding(PARAMS, 'color');
+
   const PARAMS = {
-    factor: 123,
-    title: 'hello',
-    color: '#ff0055',
+    slice: 1,
   };
 
-  pane.addBinding(PARAMS, 'factor');
-  pane.addBinding(PARAMS, 'title');
-  pane.addBinding(PARAMS, 'color');
+  const slice_slider = pane.addBinding(PARAMS, 'slice', {min: 1, max: 226, step: 1});
+
+  slice_slider.on('change', function(ev) {
+    UIData.slice = ev.value;
+  });
 
   return pane;
 }
