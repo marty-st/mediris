ea4d2fb81bc7d0b65014d2ecb3b19c85bd7a5ee5
Martin Štourač <525032@mail.muni.cz>
2026-02-07 14:09:34 +0100

    docs ui manager

--- src/ui/manager.js
+++ src/ui/manager.js
@@ -13,6 +13,12 @@ import {
   resetMouseControls 
 } from './controls.js';
 
+/**
+ * Creates an object that manages user controls and GUI. 
+ * @param {*} canvas HTML canvas element
+ * @param {*} GUIData mediator object between GUI and the rest of the application
+ * @returns UI manager object
+ */
 export function initUI(canvas, GUIData)
 {
   return {
@@ -24,12 +30,20 @@ export function initUI(canvas, GUIData)
   };
 }
 
+/**
+ * Calls functions that process user input.
+ * @param {*} UI UI manager object
+ */
 export function control(UI)
 {
   controlCamera(UI.mouse, UI.keyboard, UI.cameraControls);
   controlApp(UI.keyboard, UI.appControls);
 }
 
+/**
+ * Calls functions that reset frame-dynamic states of control objects.
+ * @param {*} UI UI manager object
+ */
 export function resetControls(UI)
 {
   resetMouseControls(UI.mouse);
