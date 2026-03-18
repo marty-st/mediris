f8d022087eb95d3e6e509ccf0ed1378fb25b66e2
Martin Štourač <525032@mail.muni.cz>
2026-03-18 11:34:10 +0100

    feat load cubemap images

--- src/file/image.js
+++ src/file/image.js
@@ -14,11 +14,26 @@ const imagePath = '/textures/';
  */
 // Inspired by:
 // https://www.youtube.com/watch?v=0nZn5YPNf5k
-export default async function loadImage(imageFileName)
+export async function loadImage(imageFileName)
 {
   const image = new Image();
   // image.addEventListener('load', () => resolve(image));
   image.src = imagePath + imageFileName;
 
   return image;
+}
+
+export async function loadImagesCubeMap(imageFolderName, format)
+{
+  // NOTE: order of the faces was established by trial and error
+  // The reason why it works with this order (and not rt, lf, up, dn, bk, ft)
+  // is unknown to me 
+  const suffixes = ["ft", "bk", "up", "dn", "rt", "lf" ];
+  let images = [];
+
+  suffixes.forEach(async (suffix) => {
+    images.push(await loadImage(imageFolderName + "/" + imageFolderName + "_" + suffix + "." + format));
+  });
+
+  return images;
 }
