4156f46cd4c2a2e5540d39c53abb033ee117fc5d
Martin Štourač <525032@mail.muni.cz>
2026-04-08 11:42:09 +0200

    style(main): move appdata variables to data.js

--- src/app/data.js
+++ src/app/data.js
@@ -1,5 +1,121 @@
 'use strict'
 
+/* GLOBAL VARIABLES */
+
+// HU units are usually defined in the range <-1000, 3000>,
+// however, the data loads in unsigned short format, so
+// the values must be offset by a constant
+const C = 1000;
+// Hounsfield units for various media
+// template: { min: 0 + C, max: 0 + C },
+const hu = {
+  air: { min: -1000 + C, max: -950 + C},
+  lungs: { min: -750 + C, max: -700 + C },
+  fat: { min: -120 + C, max: -90 + C },
+  water: { min: 0 + C, max: 0 + C },
+  muscle: { min: 35 + C, max: 55 + C },
+  softTissueContrast: { min: 100 + C, max: 300 + C },
+  boneCancellous: { min: 300 + C, max: 400 + C },
+  boneCortical: { min: -440 + C, max: 1900 + C }, // USE min: -440 for skin layer, 350 for the bone
+
+};
+
+// Transfer Function Definition
+const tf = {
+  air: { interval: hu.air, color: {r: 0, g: 0, b: 0, a: 0}, enabled: false},
+  lungs: { interval: hu.lungs, color: {r: 0.65, g: 0.35, b: 0.11, a: 0.00}, enabled: false},
+  fat: { interval: hu.fat, color: {r: 0.82, g: 0.83, b: 0.18, a: 0.00}, enabled: false},
+  water: { interval: hu.water, color: {r: 0.03, g: 0.49, b: 0.87, a: 0.00}, enabled: false},
+  muscle: { interval: hu.muscle, color: {r: 0.46, g: 0.02, b: 0.02, a: 0.00}, enabled: false},
+  softTissueContrast: { interval: hu.softTissueContrast, color: {r: 0.66, g: 0.36, b: 0.52, a: 0.00}, enabled: false},
+  boneCancellous: { interval: hu.boneCancellous, color: {r: 0.41, g: 0.66, b: 0.17, a: 0.0}, enabled: false},
+  boneCortical: { interval: hu.boneCortical, color: {r: 0.88, g: 0.88, b: 0.88, a: 1.00}, enabled: true},
+};
+
+// Lights Setup
+const lights = {
+    keyLight: {
+      position: {x: 0, y: 1, z: -1},
+      intensity: 1.0,
+      enabled: true,
+    },
+    fillLight: {
+      position: {x: 1, y: 0.75, z: 0},
+      intensity: 0.5,
+      enabled: true,
+    },
+    backLight: {
+      position: {x: 0, y: 1, z: 1},
+      intensity: 0.25,
+      enabled: true,
+    },
+};
+
+// Application time keeping
+const time = {
+  current: 0,
+  previous: 0,
+  delta: 0,
+};
+
+// Application environment data
+const environment = {
+  time: time,
+  camera: undefined,
+  viewport: undefined,    // Viewport position and dimensions
+  scene: undefined,       // Current scene object
+  lights: lights 
+};
+
+// Application settings
+const settings = {
+  uniforms: {
+    general: {
+      u_mode: 1, // 0 = Volume Data, 1 = Debug Sphere
+    },
+    rayTracing: {
+      u_step_size: 0.0025,
+      u_gradient_delta: 0.0025,
+      u_curvature_delta_multiplier: 4.0,
+      u_shading_model: 0, // 0 = Stylized, 1 = Disney, 2 = Blinn-Phong, 3 = Lambert, 4 = normal, 5 = position, 6 = cubemap
+    },
+    shadingModel: {
+      stylized: {
+        u_alpha: 0.0,
+        u_tau: 0.0,
+        u_lambda: 0.0,
+        u_mu: 0.4,
+        u_chi: 0.2,
+        u_beta: 0,
+        u_gamma: 1,
+      },
+      disney: {
+        // diffuse model
+        u_roughness: 0.5,
+        u_subsurface: 0.0,
+        u_sheen: 0.0,
+        u_sheen_tint: 0.5,
+        // specular model
+        u_specular: 0.5,
+        u_specular_tint: 0.0,
+        u_anisotropic: 0.0,
+        u_metallic: 0.0,
+        u_clearcoat: 0.0,
+        u_clearcoat_gloss: 1.0,
+      },
+      blinnPhong: {
+        u_shininess: 100.0,
+      },
+      lambert: {},
+      normal: {},
+      position: {},
+      cubemap: {},
+    },
+  },
+}
+
+/**/
+
 import { vec2, vec3, vec4 } from 'gl-matrix';
 
 /**
@@ -101,7 +217,7 @@ function initEnvironmentProperties(environment)
  * @param {*} transferFunction object that defines the transfer function
  * @returns object with application related data
  */
-export function initAppData(settings, environment, transferFunction)
+function initAppDataContent(settings, environment, transferFunction)
 {
   return {
     context: null,
@@ -109,4 +225,13 @@ export function initAppData(settings, environment, transferFunction)
     environment: initEnvironmentProperties(environment),
     transferFunction: initTransferFunctionProperties(transferFunction),
   };
+}
+
+/**
+ * Initializes the main object used for storing application data.
+ * @returns object with application related data
+ */
+export function initAppData()
+{
+  return initAppDataContent(settings, environment, tf);
 }
